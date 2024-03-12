06bd0d118ea47919e4255ac3c4d761b0455e68b0
juanc <juanc@Juan_Carlos>
2024-03-12 12:17:12 -0600

    crea un programa en java que nos pida 4 calificaciones y nos la calcule mediante una interfaz grafica

---
+++ src/promedio_inteface/Promedio_Inteface.java
@@ -0,0 +1,14 @@
+
+package promedio_inteface;
+
+
+public class Promedio_Inteface {
+
+  
+    public static void main(String[] args) {
+       Calificaciones cal  =new  Calificaciones();
+       cal.setVisible(true);
+       cal.setLocationRelativeTo(null);
+    }
+    
+}
